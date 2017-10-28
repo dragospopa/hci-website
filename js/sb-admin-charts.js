d0feeb2a85e85dde17ec6ca987a27d20987e2277
dragospopa <dragos.popa21@yahoo.com>
2017-10-28 23:38:06 +0100

    first page done-ish + a tonne of styling changed

--- js/sb-admin-charts.js
+++ js/sb-admin-charts.js
@@ -95,14 +95,14 @@ var myLineChart = new Chart(ctx, {
   }
 });
 // -- Pie Chart Example
-var ctx = document.getElementById("myPieChart");
+var ctx = document.getElementById("myPie");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["Blue", "Yellow", "Green"],
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+      data: [100, 250, 300],
+      backgroundColor: ['#007bff', '#ffc107', '#28a745'],
     }],
   },
 });
